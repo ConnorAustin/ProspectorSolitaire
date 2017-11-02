4510270cf57a3e872cead3ecf963bae20ca23bb8
GameDev <arcademissile@gmail.com>
2017-11-02 16:46:47 -0500

    Worked on card covering other cards

--- Assets/Card.cs
+++ Assets/Card.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class Card : MonoBehaviour {
 	Sprite cardSprite;
+	Sprite cardbackSprite;
 	string suit;
 	int rank; // From 0 (which is a 2) upwards to Ace
 
+	[HideInInspector]
+	public List<Card> coveredBy = new List<Card> ();
+
+	public bool faceUp = false;
+
 	void Start () {
+		cardbackSprite = GetComponent<SpriteRenderer> ().sprite;
+	}
+
+	public void Flip() {
+		faceUp = !faceUp;
+		var s = GetComponent<SpriteRenderer> ();
+		if (faceUp) {
+			s.sprite = cardSprite;
+		} else {
+			s.sprite = cardbackSprite;
+		}
 	}
 
 	public void SetCard(int rank, string suit) {
--- Assets/Prospector.cs
+++ Assets/Prospector.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Prospector : MonoBehaviour {
+	public Transform drawPile;
+
 	void Start () {
 		
 	}
@@ -28,19 +30,27 @@ public class Prospector : MonoBehaviour {
 		for (int i = 0; i < 9; i++) {
 			cards [c].transform.position = new Vector3 (-8.0f + i * step, transform.position.y - 1, transform.position.z + 0.2f);
 			cards [c].GetComponent<SpriteRenderer> ().sortingOrder = 3;
-			c++;
+			cards [c].coveredBy.Add (cards[i]);
+			cards [c].coveredBy.Add (cards[i + 1]);
+			c++; 
 		}
 
 		// 3rd Layer
 		float offset = -7.0f;
+		int coverOffset = 0;
 		for (int i = 0; i < 6; i++) {
 			cards [c].transform.position = new Vector3 (offset, transform.position.y - 2, transform.position.z + 0.1f);
 			cards [c].GetComponent<SpriteRenderer> ().sortingOrder = 2;
 
+			cards [c].coveredBy.Add (cards[10 + coverOffset]);
+			cards [c].coveredBy.Add (cards[10 + 1 + coverOffset]);
+
 			if (i % 2 == 1) {
 				offset += 2 * step;
+				coverOffset += 2;
 			} else {
 				offset += step;
+				coverOffset += 1;
 			}
 			c++;
 		}
@@ -49,11 +59,25 @@ public class Prospector : MonoBehaviour {
 		for (int i = 0; i < 3; i++) {
 			cards [c].transform.position = new Vector3 (-6.0f + i * 6, transform.position.y - 3, transform.position.z);
 			cards [c].GetComponent<SpriteRenderer> ().sortingOrder = 1;
+			cards [c].coveredBy.Add (cards[19 + 2 * i]);
+			cards [c].coveredBy.Add (cards[19 + 1 + 2 * i]);
+			c++;
+		}
+
+		// Draw pile
+		while(c != cards.Count) {
+			cards [c].transform.position = drawPile.position;
 			c++;
 		}
 	}
 	
 	void Update () {
-		
+		if (Input.GetMouseButtonDown (0)) {
+			var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray.origin, ray.direction, out hit, 2000.0f, LayerMask.GetMask ("Card"))) {
+				Card card = hit.collider.gameObject.GetComponent<Card> ();
+			}
+		}
 	}
 }
